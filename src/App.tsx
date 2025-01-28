b78edd091a24158ed68cc2cbcdf4f7a8cd732e52
PrymushAnton <anton.primush123663@gmail.com>
2025-01-28 18:55:33 +0200

    Logic of likes was changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,94 @@
-
 import { Layout } from "./shared/Layout/Layout";
 import { Main } from "./shared/Main/Main";
 import { Header } from "./shared/Header/Header";
 import { Footer } from "./shared/Footer/Footer";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { PostListPage } from "./pages/PostListPage/PostListPage";
 import { PostPage } from "./pages/PostPage/PostPage";
 import { MainPage } from "./pages/MainPage/MainPage";
 import { IPost } from "./hooks/usePosts";
 import { createContext, useEffect, useState } from "react";
 
-type SetFunction = (isButtonClicked: boolean) => void
+type SetFunction = (isButtonClicked: boolean) => void;
 
-
-interface ILikedPostsContext{
-    likedPosts: IPost[],
-    clickOnLikedPost: (
-        likedPost: IPost,
-        setFunction: SetFunction
-    ) => void
+interface ILikedPostsContext {
+	likedPosts: IPost[];
+	checkStatus: (likedPost: IPost) => boolean;
+	likePost: (likedPost: IPost) => void;
+	unlikePost: (likedPost: IPost) => void;
 }
 
 const initialValue: ILikedPostsContext = {
-    likedPosts: [],
-    clickOnLikedPost: (
-        likedPost: IPost,
-        setFunction: SetFunction
-    ) => {}
-}
+	likedPosts: [],
+	checkStatus: (likedPost: IPost) => false,
+	likePost: (likedPost: IPost) => {},
+	unlikePost: (likedPost: IPost) => {},
+};
 
-export const likedPostsContext = createContext<ILikedPostsContext>(initialValue)
+export const likedPostsContext =
+	createContext<ILikedPostsContext>(initialValue);
+
+export function App() {
+	const [likedPosts, setLikedPosts] = useState<IPost[]>([]);
 
 
-export function App(){
+	function checkStatus(likedPost: IPost) {
+		if (likedPosts.find((post) => likedPost.id === post.id)) {
+			return true;
+		}
+		return false;
+	}
 
-    const [likedPosts, setLikedPosts] = useState<IPost[]>([])
+	function likePost(likedPost: IPost) {
+		if (!checkStatus(likedPost)) {
+			const tempArray = [...likedPosts, likedPost];
+			setLikedPosts(tempArray);
+		} else {
+			const tempArray = [...likedPosts];
+			const index = tempArray.findIndex(
+				(post) => post.id === likedPost.id
+			);
+			tempArray.splice(index, 1);
+			setLikedPosts(tempArray);
+		}
+	}
 
-    // useEffect(() => {
-    //     console.log(likedPosts)
-    // }, [likedPosts])    
+	function unlikePost(likedPost: IPost) {
+		if (checkStatus(likedPost)) {
+			const tempArray = [...likedPosts];
+			const index = tempArray.findIndex(
+				(post) => post.id === likedPost.id
+			);
+			tempArray.splice(index, 1);
+			setLikedPosts(tempArray);
+		}
+	}
 
-    function clickOnLikedPost(likedPost: IPost, setFunction: SetFunction){
-        if (!likedPosts.find(post => likedPost.id === post.id)){
-            const tempArray = [
-                ...likedPosts,
-                likedPost
-            ]
-            setFunction(true)
-            setLikedPosts(tempArray)
-        } else {
-            const tempArray = [...likedPosts]
-            const index = tempArray.findIndex(post => post.id === likedPost.id)
-            tempArray.splice(index, 1);
-            setFunction(false)
-            setLikedPosts(tempArray)
-        }
-    }
-
-    return (
-        <likedPostsContext.Provider value={{likedPosts: likedPosts, clickOnLikedPost: clickOnLikedPost}}>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Layout></Layout>}>
-                        <Route path="/posts" element={<PostListPage></PostListPage>}></Route>
-                        <Route path="/post/:id" element={<PostPage></PostPage>}></Route>
-                        <Route path="/" element={<MainPage></MainPage>}></Route>
-                    </Route>
-                </Routes>
-            </BrowserRouter>
-        </likedPostsContext.Provider>
-    )
+	return (
+		<likedPostsContext.Provider
+			value={{
+				likedPosts: likedPosts,
+				likePost: likePost,
+				checkStatus: checkStatus,
+				unlikePost: unlikePost,
+			}}
+		>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Layout></Layout>}>
+						<Route
+							path="/posts"
+							element={<PostListPage></PostListPage>}
+						></Route>
+						<Route
+							path="/post/:id"
+							element={<PostPage></PostPage>}
+						></Route>
+						<Route path="/" element={<MainPage></MainPage>}></Route>
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</likedPostsContext.Provider>
+	);
 }
